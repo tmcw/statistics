f4488db4cdc834fb40e13e10026ef8c5fc7692a0
ergenekonyigit <ergenekon.yigit@trendyol.com>
2020-05-19 23:03:12 +0300

    mode

---
+++ src/mode.test.ts
@@ -0,0 +1,15 @@
+import { assertEquals, assertThrows } from 'https://deno.land/std/testing/asserts.ts';
+import { mode } from "./mode.ts";
+
+Deno.test("mode", () => {
+  assertEquals(mode([2, 4, 0, 1, 3, -1, 1]), 1);
+  assertEquals(mode([67, 1, 131, 34, 56, 67]), 67);
+
+  assertThrows(
+    (): void => {
+      mode([]);
+    },
+    Error,
+    "mode requires at least one data point"
+  );
+});
