66264a4d61a4e8b54457e27010ed82277a45d100
ergenekonyigit <ergenekon.yigit@trendyol.com>
2020-05-19 23:03:03 +0300

    modeSorted

---
+++ src/mode_sorted.test.ts
@@ -0,0 +1,15 @@
+import { assertEquals, assertThrows } from 'https://deno.land/std/testing/asserts.ts';
+import { modeSorted } from "./mode_sorted.ts";
+
+Deno.test("modeSorted", () => {
+  assertEquals(modeSorted([-1, 0, 1, 1, 2, 3, 4]), 1);
+  assertEquals(modeSorted([1, 34, 56, 67, 67, 131]), 67);
+
+  assertThrows(
+    (): void => {
+      modeSorted([]);
+    },
+    Error,
+    "mode requires at least one data point"
+  );
+});
